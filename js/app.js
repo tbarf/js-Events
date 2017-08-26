60ac9b94d85e1f32eb1daa48f46ee8f9cccd566d
tbarf <30032737+tbarf@users.noreply.github.com>
2017-08-25 22:00:17 -1000

    first done second done

--- js/app.js
+++ js/app.js
@@ -6,16 +6,30 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+function moreContent(){
+var getmore = document.getElementById("more").innerHTML = bacon;
+
+}
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
-
+function lessContent(){
+	var showLess = document.getElementById("less");
+	if (showLess.style.display !== 'none') {
+        showLess.style.display = 'none';
+    }
+    else {
+        showLess.style.display = 'block';
+    }
+}
 
 //3. Tacocat, The Original Palindrome King
-/*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
-
+/*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increase the font size to 150%*/
+function zoom(){
+	var lz = document.getElementById('biggie');
+}
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
